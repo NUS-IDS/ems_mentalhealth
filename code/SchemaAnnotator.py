4c3740bf5b3694e8aabbbae4674fa104a0eff0b2
gud111 <58678112+gud111@users.noreply.github.com>
2026-04-08 17:53:51 +0800

    Add files via upload

--- code/SchemaAnnotator.py
+++ code/SchemaAnnotator.py
@@ -8,7 +8,7 @@ Created on Wed Feb  4 12:19:16 2026
 from Common import getChatGPTResponse, getGeminiResponse, parseJSONWithKeys
 
 
-def loadSchemaDefs(name2def_tsv="/home/sdas/npj_calba/ST_resources/schema_defs.tsv"):
+def loadSchemaDefs(name2def_tsv="../resources/schema_defs.tsv"):
     
     lines =  open (name2def_tsv, "r").readlines()
     name2def={}
